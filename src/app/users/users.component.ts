2737626234123f01a01bf46ced0999cbad086c0a
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-27 12:11:18 +0100

    Enhance users component by adding id input and emitting select event on user selection

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -8,21 +8,23 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  @Input({ required: true }) id!: string
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-// avatar = input.required<string>();
-// name = input.required<string>(); 
+  @Output() select = new EventEmitter();
+  // avatar = input.required<string>();
+  // name = input.required<string>(); 
 
-// imagePath = computed(() => {
-//   return 'assets/users/' + this.avatar();
-// });
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-    
+    this.select.emit(this.id);
   }
 }
 
