98a34c1727933b351db1c44142f828e451a12854
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-27 12:32:14 +0100

    Fix users component output declaration by re-enabling EventEmitter for select event

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -11,8 +11,8 @@ export class UsersComponent {
   @Input({ required: true }) id!: string
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
   // avatar = input.required<string>();
   // name = input.required<string>(); 
 
