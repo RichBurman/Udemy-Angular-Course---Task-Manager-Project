4b5be79b34894527d0ee746acae7ddf4e9c6cc61
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-27 10:33:58 +0100

    Refactor users component to include computed and input imports for future enhancements. Added in comments in the user component.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -10,13 +10,19 @@ import { Component, Input } from '@angular/core';
 export class UsersComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+// avatar = input.required<string>();
+// name = input.required<string>(); 
+
+// imagePath = computed(() => {
+//   return 'assets/users/' + this.avatar();
+// });
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-
+    
   }
 }
 
