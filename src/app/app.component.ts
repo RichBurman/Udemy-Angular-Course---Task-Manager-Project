3200b93195b118a5a5df304f0e99db6c3f91ec3a
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-05-05 11:34:51 +0100

    Refactor users list rendering in app component to use a loop for dynamic user display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,9 @@ export class AppComponent {
   selectedUserId = 'u1';
 
   get selectedUser() {
-    
     return this.users.find((user) => user.id === this.selectedUserId)!;
   }
-  
+
   onSelectUser(id: string) {
     this.selectedUserId = id;
   }
