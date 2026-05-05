a686a96a3e09f75d109aff88f6a0818c9032a84b
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-05-05 13:04:55 +0100

    Add selected input binding to users component and update user selection logic

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -11,6 +11,7 @@ import { User } from './user.model';
 })
 export class UsersComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
 
