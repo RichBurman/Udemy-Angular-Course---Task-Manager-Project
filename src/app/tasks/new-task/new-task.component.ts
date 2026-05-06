b95e1395f8f6610471be358b1784813861c47ba9
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-05-06 15:58:07 +0100

    Refactor task management by integrating TaskService for task handling and updating NewTaskComponent to utilize service methods for task addition and removal.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NewTaskData } from '../task/task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,26 +11,31 @@ import { NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-@Output() cancel = new EventEmitter<void>()
-@Output() add = new EventEmitter<NewTaskData>()
-enteredTitle = '';
-enteredSummary = '';
-enteredDate = '';
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>()
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
-// how to set up with signals. 
-// enteredTitle = signal('');
-// enteredSummary = signal('');
-// enteredDate = signal('');
+  private taskService = inject(TaskService)
+
+  // how to set up with signals. 
+  // enteredTitle = signal('');
+  // enteredSummary = signal('');
+  // enteredDate = signal('');
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.taskService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    })
+    },
+      this.userId
+    );
+    this.close.emit();
   }
 }
